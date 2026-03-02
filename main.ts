8c031d72f617b72a93d82bb7fdc239c5127679f3
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-02 13:08:39 -0500

    d

--- main.ts
+++ main.ts
@@ -2,15 +2,15 @@
  *
  * Created by: Evan
  * Created on: Feb 2026
- * This program ...
+ * This program will show the temperature
 */
 
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
+//temperature
 input.onButtonPressed(Button.A, function () {
     const temperature = input.temperature()
     basic.clearScreen()
     basic.showString("The temperature is:" + (temperature) + "C.")
 })
-
