b0c9f2507cbd8cc764e75daa86642e19347f76f3
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-04-02 16:50:26 +0000

    wadfsdaes

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,6 +6,8 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
+
+
 # Show a happy face on start
 display.clear()
 display.show(Image.HAPPY)
@@ -14,12 +16,12 @@ while True:
     # Check if Button A is pressed
     if button_a.was_pressed():
         # Get the temperature
-        temp = temperature()
+        temperature = temperature()
 
         display.clear()
         # Scroll the temperature message
         # We use str(temp) to combine the number with the text
-        display.scroll("The temperature is: " + str(temp) + "C.")
+        display.scroll("The temperature is: " + str(temperature) + "C.")
 
         # Show the happy face again after scrolling
         display.show(Image.HAPPY)
